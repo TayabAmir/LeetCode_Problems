3020632731f6cba30dbd146761f0d42e1514240c
TayabAmir <144408356+TayabAmir@users.noreply.github.com>
2024-06-06 15:23:05 +0500

    Time: 0 ms (100%), Space: 7.3 MB (66.19%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,7 +4,7 @@ public:
         int no_of_Ones = 0;
         while(n)
         {
-            if(n & 1) no_of_Ones++;
+            no_of_Ones += (n & 1);
             n >>= 1;
         }
         return no_of_Ones;
