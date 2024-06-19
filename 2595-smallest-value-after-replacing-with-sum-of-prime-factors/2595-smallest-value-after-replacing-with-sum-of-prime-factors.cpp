0154536858829bd10c5d3503e7a4cc4856b01788
TayabAmir <144408356+TayabAmir@users.noreply.github.com>
2024-06-19 12:59:11 +0500

    Time: 3 ms (58.59%), Space: 7.1 MB (55.96%) - LeetHub

---
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/2595-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumofPF(int n){
+        int sum = 0;
+        while (n % 2 == 0) {
+            sum += 2;
+            n /= 2;
+        }
+        for (int i = 3; i <= n; i += 2) { 
+            while (n % i == 0) {
+                sum += i;
+                n /= i;
+            }
+        }
+        return sum;
+    }
+    int smallestValue(int n) {
+        while(true){
+            if(n == sumofPF(n))
+                return n;
+            n = sumofPF(n);
+        }
+        return -1;
+    }
+};
