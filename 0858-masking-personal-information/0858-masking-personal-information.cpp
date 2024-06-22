d209e7aaddac0f797608bc83dde1c2922d36c96f
TayabAmir <144408356+TayabAmir@users.noreply.github.com>
2024-06-22 14:06:57 +0500

    Time: 3 ms (35.87%), Space: 7.4 MB (26.09%) - LeetHub

---
+++ 0858-masking-personal-information/0858-masking-personal-information.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int isEmail(string s){
+        for(int i=0;i<s.length();i++)
+            if(s[i] == '@')
+                return i;
+        return -1;
+    }
+    string lower(string s){
+        for(int i=0;i<s.length();i++)
+            s[i] = tolower(s[i]);
+        return s;
+    }
+    string getDigits(string s){
+        string res = "";
+        for(char c: s){
+            if(c >= '0' && c <= '9'){
+                res += c;
+            } 
+        }
+        return res;
+    }
+    string maskPII(string s) {
+        int email = isEmail(s);
+        string res = "";
+        if(email != -1){
+            s = lower(s),res="*****";
+            res=string(1,s[0])+res+string(1,s[email-1])+'@'+ s.substr(email+1, s.size()-email+1);
+        }
+        else{
+            s = getDigits(s);
+            res="***-***-"+s.substr(s.length()-4,4);
+            if(s.length() == 11)
+                res = "+*-" + res;
+            else if(s.length() == 12)
+                res = "+**-" + res;
+            else if(s.length() == 13)
+                res = "+***-" + res;
+        }
+        return res;
+    }
+};
