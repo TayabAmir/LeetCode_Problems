80ff5e617fc135fa2c2bf6db89cda1e3e08fa077
TayabAmir <144408356+TayabAmir@users.noreply.github.com>
2024-07-06 23:02:26 +0500

    Time: 0 ms (100%), Space: 7.3 MB (30.58%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -3,10 +3,7 @@ public:
     int passThePillow(int n, int time) {
         int i = 1,direction = 1;
         while(time--){
-            if(direction==1)
-                i++;
-            else
-                i--;
+            (direction==1) ? i++ : i--;
             if(i==n) direction=-1;
             else if(i==1) direction=1;
         }
