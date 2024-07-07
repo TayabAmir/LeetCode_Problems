1bd645e473da41ad83d0eb4d960cac6846a9ebd9
TayabAmir <144408356+TayabAmir@users.noreply.github.com>
2024-07-07 23:01:09 +0500

    Time: 188 ms (33.72%), Space: 179.2 MB (98.21%) - LeetHub

--- 3420-find-occurrences-of-an-element-in-an-array/3420-find-occurrences-of-an-element-in-an-array.cpp
+++ 3420-find-occurrences-of-an-element-in-an-array/3420-find-occurrences-of-an-element-in-an-array.cpp
@@ -3,13 +3,9 @@ public:
     vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& q, int x) {
         vector<int> helper;
         for(int i = 0; i < nums.size(); i++)
-            if(nums[i] == x)
-                helper.push_back(i);
+            if(nums[i] == x) helper.push_back(i);
         for(int i = 0; i < q.size(); i++)
-            if(q[i] <= helper.size())
-                q[i] = helper[q[i]-1];
-            else
-                q[i] = -1;
+            q[i] = (q[i] <= helper.size()) ? helper[q[i]-1] : q[i] = -1;
         return q;
     }
 };
