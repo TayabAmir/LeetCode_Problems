f9b06927c8d25b8b30e244aa7e45f52cac62b755
TayabAmir <144408356+TayabAmir@users.noreply.github.com>
2024-07-17 14:26:45 +0500

    Time: 565 ms (12.2%), Space: 340.1 MB (8.69%) - LeetHub

--- 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.cpp
+++ 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     static bool comp(string a, string b){
         if(a.length() == b.length())
-            return a < b;
-        return a.length() < b.length();
+            return a > b;
+        return a.length() > b.length();
     }
     string kthLargestNumber(vector<string>& nums, int k) {
         sort(nums.begin(),nums.end(),comp);
-        return nums[nums.size()-k];
+        return nums[k-1];
     }
 };
