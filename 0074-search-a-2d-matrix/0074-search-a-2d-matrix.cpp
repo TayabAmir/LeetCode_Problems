d8a983b5c238bfbd76e3490ed76385b59bbdd5b1
TayabAmir <144408356+TayabAmir@users.noreply.github.com>
2024-07-24 17:46:01 +0500

    Time: 0 ms (100%), Space: 12.1 MB (15.68%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -13,7 +13,6 @@ public:
                 }
                 break;
             } else if(target == matrix[i][n-1]) return true;
-            else if(matrix[i][0] > target) break;
         }
         return false;
     }
