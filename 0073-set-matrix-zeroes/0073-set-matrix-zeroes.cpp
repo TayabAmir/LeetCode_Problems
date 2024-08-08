e4d93b63e0678e5e014cc553b6ccccb4d22badcf
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-08 13:26:39 +0500

    Time: 8 ms (76.81%), Space: 17 MB (17.9%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -8,15 +8,11 @@ public:
     }
     void setZeroes(vector<vector<int>>& matrix) {
         vector<pair<int,int>> v;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j] == 0){
+        for(int i=0;i<matrix.size();i++)
+            for(int j=0;j<matrix[i].size();j++)
+                if(matrix[i][j] == 0)
                     v.push_back(make_pair(i,j));
-                }
-            }
-        }
-        for(auto p : v){
+        for(auto p : v)
             setZeros(matrix,p.first,p.second);
-        }
     }
 };
