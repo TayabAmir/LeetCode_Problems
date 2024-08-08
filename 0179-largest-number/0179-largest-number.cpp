537f94584e0c87ab02942bd2d98677489d86d1ec
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-08 10:29:42 +0500

    Time: 14 ms (9.71%), Space: 15.6 MB (72.77%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     static bool comp(int a,int b){
         string s1 = to_string(a), s2 = to_string(b);
-        if(s1[0] > s2[0]) return true;
-        if(s1+s2 > s2+s1) return true;
-        return false;
+        return s1+s2 > s2+s1;
     }
     string largestNumber(vector<int>& arr) {
         sort(arr.begin(),arr.end(),comp);
         if(arr[0] == 0) return "0";
         string l = "";
-        for (int num : arr)
-            l += to_string(num);
+        for (int i = 0; i < arr.size(); i++)
+            l += to_string(arr[i]);
         return l;
     }
 };
