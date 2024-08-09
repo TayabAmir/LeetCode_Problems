d1e0900ababbe7d6b9c5db726fce6bb168d21c30
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-09 10:49:46 +0500

    Time: 186 ms (51.76%), Space: 81.4 MB (42.71%) - LeetHub

--- 2378-sender-with-largest-word-count/2378-sender-with-largest-word-count.cpp
+++ 2378-sender-with-largest-word-count/2378-sender-with-largest-word-count.cpp
@@ -5,17 +5,13 @@ public:
         int k = 0;
         for(string message : messages){
             int spaces = 1;
-            for(char c : message)
-                if(c == ' ') spaces++;
+            for(char c : message) if(c == ' ') spaces++;
             m[senders[k++]] += spaces;
         }
-        int max = -1;
+        k = -1;
         string sender = "";
-        for(auto pair : m){
-            if(pair.second >= max){
-                max = pair.second, sender = pair.first;
-            }
-        }
+        for(auto pair : m)
+            if(pair.second >= k) k = pair.second, sender = pair.first;
         return sender;
     }
 };
