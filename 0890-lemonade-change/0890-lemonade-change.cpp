a7b6dc5fd4129f739a409c17ab7d492443947e47
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-15 21:12:10 +0500

    Time: 68 ms (74.03%), Space: 85.9 MB (63.29%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int _5Count = 0, _10Count = 0, _20Count = 0;
+        for(int i = 0; i < bills.size(); ++i){
+            if(bills[i] == 5){
+                ++_5Count;
+            } else if(bills[i] == 10){
+                ++_10Count;
+                if(_5Count == 0) return false;
+                else  _5Count--;
+            } else {
+                ++_20Count;
+                if(_10Count == 0 && _5Count >= 3)
+                    _5Count -= 3;
+                else if(_10Count >= 1 && _5Count >= 1)
+                    --_5Count,--_10Count;
+                else
+                    return false; 
+            }
+        }
+        return true;
+    }
+};
