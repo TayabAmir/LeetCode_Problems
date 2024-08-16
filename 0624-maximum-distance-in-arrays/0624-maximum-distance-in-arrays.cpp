52a0a435f18a7393d936f393dcec186eb8b1054d
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-16 19:34:21 +0500

    Time: 267 ms (12.26%), Space: 122 MB (8.96%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int smallest = INT_MAX, largest = INT_MIN,secondLargest = INT_MIN, secondSmallest = INT_MAX,rowLargest = -1, rowSmallest = -1, i = 0;
+        int smallest = INT_MAX, largest = INT_MIN,secondLargest = INT_MIN, secondSmallest = INT_MAX,rowLargest = -1, rowSmallest = -1;
 
-        for (auto v : arrays) {
-            if (v[0] < smallest) {
-                secondSmallest = smallest;
-                smallest = v[0];
-                rowSmallest = i;
-            } else if (v[0] < secondSmallest) {
-                secondSmallest = v[0];
-            }
-            
-            if (v.back() > largest) {
-                secondLargest = largest;
-                largest = v.back();
-                rowLargest = i;
-            } else if (v.back() > secondLargest) {
-                secondLargest = v.back();
-            }
-            ++i;
+        for (int i = 0; i < arrays.size();i++) {
+            vector<int> v = arrays[i];
+            if (v[0] < smallest) secondSmallest = smallest, smallest = v[0], rowSmallest = i;
+            else if (v[0] < secondSmallest)  secondSmallest = v[0];
+            if (v.back() > largest) secondLargest = largest, largest = v.back(), rowLargest = i;
+            else if (v.back() > secondLargest) secondLargest = v.back();
         }
-
-        if (rowLargest != rowSmallest) {
+        if (rowLargest != rowSmallest) 
             return abs(largest - smallest);
-        }
-
         return max(abs(secondLargest - smallest), abs(largest - secondSmallest));
     }
 };
