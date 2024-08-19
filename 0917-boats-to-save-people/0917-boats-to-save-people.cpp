955926f7dd4b5f62f0931655befee77fa5c8e3e4
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-19 18:00:35 +0500

    Time: 61 ms (35.71%), Space: 45.6 MB (63.68%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int boats = 0, left = 0, right = people.size()-1;
+        while(left <= right && ++boats)
+            (people[left] + people[right] <= limit) ? left++,right-- : right--;
+        return boats;
+    }
+};
