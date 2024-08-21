9bcbbe8d2da56b16eaeb57f318af8cf2bd499c2f
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-21 14:52:17 +0500

    Time: 8 ms (86.4%), Space: 7.6 MB (19.73%) - LeetHub

--- 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     long long waysToBuyPensPencils(int total, int cost1, int cost2) {
         long long ways = 0;
-        int penCount = 0;
-        while(penCount <= total / cost1){
-            int pencils = (total - cost1 * penCount) / cost2;
-            ways += (pencils + 1), penCount++;
-        }
+        for(int penCount = 0; penCount <= total / cost1; penCount++)
+            ways = ways + (total - cost1 * penCount) / cost2 + 1 ;
         return ways;
     }
 };
