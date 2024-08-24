4adbfe55e74cb8a67bb1cd287b14b10ebf531b1b
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-24 20:15:33 +0500

    Time: 64 ms (58.09%), Space: 57.5 MB (42.53%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -12,31 +12,32 @@
  */
 class Solution {
 public:
-    // void insertHelper(TreeNode *root, TreeNode *node_to_be_inserted){
-    //     if(!root){
-    //         root = node_to_be_inserted;
-    //         return;
-    //     }
-    //     if(root->val > node_to_be_inserted->val){
-    //         insertHelper(root->left, node_to_be_inserted);
-    //     } else {
-    //         insertHelper(root->right, node_to_be_inserted);
-    //     }
-    // }
-    TreeNode* insertHelper(TreeNode* root, int data) {
-        if (!root) {
-            root = new TreeNode(data);
-            return root;
+    void insertHelper(TreeNode *&root, TreeNode *node_to_be_inserted){
+        if(!root){
+            root = node_to_be_inserted;
+            return;
         }
-        if (root->val > data) {
-            root->left = insertHelper(root->left, data);
+        if(root->val > node_to_be_inserted->val){
+            insertHelper(root->left, node_to_be_inserted);
         } else {
-            root->right = insertHelper(root->right, data);
+            insertHelper(root->right, node_to_be_inserted);
         }
-        return root;
     }
+    // TreeNode* insertHelper(TreeNode* root, int data) {
+    //     if (!root) {
+    //         root = new TreeNode(data);
+    //         return root;
+    //     }
+    //     if (root->val > data) {
+    //         root->left = insertHelper(root->left, data);
+    //     } else {
+    //         root->right = insertHelper(root->right, data);
+    //     }
+    //     return root;
+    // }
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        root = insertHelper(root, val);
+        // root = insertHelper(root, val);
+        insertHelper(root, new TreeNode(val));
         return root;
     }
 };
