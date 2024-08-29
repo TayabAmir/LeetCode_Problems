b726f8002bd898d0d39c16d0d3392d8aaf5303ff
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-29 19:42:46 +0500

    Time: 217 ms (16.51%), Space: 83.9 MB (38.84%) - LeetHub

--- 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.cpp
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-        priority_queue<int> pq;
         map<int,int> m;
         for(int i : arr) m[i]++;
-        for(auto p : m) 
-            pq.push(p.second);
+
+        priority_queue<int> pq;
+        for(auto p : m) pq.push(p.second);
+        
         int sum = 0, size = pq.size();
         while(!pq.empty()){
-            if(sum < arr.size()/2) sum += pq.top();
-            else break;
+            if(sum < arr.size()/2) 
+                sum += pq.top();
+            else 
+                break;
             pq.pop();
         }
-        return size-pq.size();
+        return size - pq.size();
     }
 };
