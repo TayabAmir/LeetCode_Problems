c8edf672816499c751ac78c3259ed5d5abff4a9e
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-31 00:31:56 +0500

    Time: 17 ms (56.56%), Space: 18.3 MB (65.42%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,31 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class compare{
+public:
+    bool operator()(ListNode* a, ListNode* b){
+        return a->val > b->val;
+    }
+};
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* current = dummy;
-        while(l1 && l2){
-            if(l1->val < l2->val){
-                current->next = l1;
-                l1 = l1->next;
-            } else {
-                current->next = l2;
-                l2 = l2->next;
-            }
-            current = current->next;
-        }
-        if(l1)  current->next = l1;
-        if(l2)  current->next = l2;
-        return dummy->next;
-    }
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size() == 0) return NULL;
-        if(lists.size() == 1) return lists[0];
-        ListNode* head = lists[0];
-        for(int i = 1; i < lists.size(); i++)
-            head = mergeTwoLists(head,lists[i]);
-        return head;
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        for(auto list : lists)
+            if(list) pq.push(list);
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode *res = dummy;
+
+        while(!pq.empty()){
+            ListNode *top  = pq.top();
+            pq.pop();
+
+            res->next = top, res= res->next;
+            if(top->next){
+                pq.push(top->next);
+            }
+        }
+
+        return dummy->next;
+        
     }
 };
