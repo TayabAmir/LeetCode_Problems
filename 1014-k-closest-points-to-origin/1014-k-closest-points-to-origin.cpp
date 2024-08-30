140f099ee5f2a4832a30c93ef5063e0e88da80a9
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-08-30 14:05:11 +0500

    Time: 218 ms (15.89%), Space: 83.3 MB (25.16%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double calculateDistance(int x, int y){
+        return sqrt((x*x)+(y*y));
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<double, vector<int>>> pq;
+
+        for(auto v : points){
+            pq.push(make_pair(calculateDistance(v[0], v[1]), v));
+            if(pq.size() > k) pq.pop();
+        }
+
+        points.clear();
+
+        while(!pq.empty()){
+            points.push_back(pq.top().second);
+            pq.pop();
+        }
+        return points;
+    }
+};
