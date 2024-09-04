c114b81cea3d2b7731088c5b3871dc74cd66893c
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-09-04 10:34:48 +0500

    Time: 4 ms (74.55%), Space: 10.6 MB (41.22%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -3,20 +3,19 @@ public:
     string countAndSay(int n) {
         if (n == 1) return "1";
         string output = "1";
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; ++i) 
             output = helper(output);
-        }
         return output;
     }
     string helper(string s){
         string output = "";
-        for(int i=0;i<s.length();){
+        int i=0;
+        while(i<s.length()){
             int count = 0;
             char c = s[i];
-            while(i<s.length() && s[i] == c) 
-                count++,i++;
-            output += to_string(count);
-            output += c;
+            while(i < s.length() && s[i] == c) 
+                count++, i++;
+            output += (to_string(count) + c);
         }
         return output;
     }
