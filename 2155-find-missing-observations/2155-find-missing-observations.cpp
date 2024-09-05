9464d68ad796b09cf981af7308bdfd5e442a3ba8
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-09-05 13:40:51 +0500

    Time: 95 ms (67.65%), Space: 126.9 MB (8.58%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -5,14 +5,12 @@ public:
         for(int ele : rolls)
             sum += ele;
         int remaining = (mean * (rolls.size() + n)) - sum;
-        if(remaining > n * 6 || remaining <= 0) return {};
 
         vector<int> ans;
         while(n){
             sum = remaining / n--;
-            if(sum <= 0) return {};
-            ans.push_back(sum);
-            remaining -= sum;
+            if(sum <= 0 || sum > 6) return {};
+            ans.push_back(sum), remaining -= sum;
         }
         return ans;
     }
