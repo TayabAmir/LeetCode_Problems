4e860a8ad7f07075db5de9668bb1a090c405e1df
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-09-13 22:19:13 +0500

    Time: 0 ms (100%), Space: 15.3 MB (62.57%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    vector<string> ans;
-    void solve(string s, int openBracket, int closeBracket, int n){
+    void solve(vector<string> &ans,string s, int openBracket, int closeBracket){
         if(openBracket == 0 && closeBracket == 0){
             ans.push_back(s);
             return;
         }
         if(openBracket != 0)
-            solve(s+'(', openBracket-1, closeBracket, n);
+            solve(ans, s+'(', openBracket-1, closeBracket);
         if(closeBracket != 0 && closeBracket > openBracket)
-            solve(s+')', openBracket, closeBracket-1, n);
+            solve(ans, s+')', openBracket, closeBracket-1);
     }
     vector<string> generateParenthesis(int n) {
-        solve("", n, n, n);
+        vector<string> ans;
+        solve(ans, "", n, n);
         return ans;
     }
 };
