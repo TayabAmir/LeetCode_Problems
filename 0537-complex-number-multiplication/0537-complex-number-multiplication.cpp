3837c8936d47d4f14aeb9c4c3cdd4eb8e1dfc62c
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-10-02 12:04:31 +0500

    Time: 0 ms (100%), Space: 7.5 MB (93.06%) - LeetHub

--- 0537-complex-number-multiplication/0537-complex-number-multiplication.cpp
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.cpp
@@ -2,26 +2,18 @@ class Solution {
 public:
     int getRealNumber(string s, int &i){
         string realNo = "";
-        while(s[i] != '+'){
-            realNo += s[i];
-            i++;
-        }
+        while(s[i] != '+')
+            realNo += s[i],i++;
         return stoi(realNo);
     }
     int getImaginaryNumber(string s, int i){
         string imaginaryNo = "";
-        while(s[i] != 'i'){
-            imaginaryNo += s[i];
-            i++;
-        }
+        while(s[i] != 'i')
+            imaginaryNo += s[i], i++;
         return stoi(imaginaryNo);
     }
     string complexNumberMultiply(string num1, string num2) {
-        int idx1 = 0, idx2 = 0;
-        int r1 = getRealNumber(num1, idx1);
-        int r2 = getRealNumber(num2, idx2);
-        int i1 = getImaginaryNumber(num1, idx1+1);
-        int i2 = getImaginaryNumber(num2, idx2+1);
+        int idx1 = 0, idx2 = 0, r1 = getRealNumber(num1, idx1), r2 = getRealNumber(num2, idx2), i1 = getImaginaryNumber(num1, idx1+1), i2 =  getImaginaryNumber(num2, idx2+1);
         return to_string(r1*r2-i1*i2) + "+" + to_string(r1*i2+r2*i1) + "i";
     }
 };
