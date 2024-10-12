53a7948c751b45c8b2d46c5801e511aa6b0d52d6
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-10-12 23:14:03 +0500

    Time: 4 ms (17.15%), Space: 7.8 MB (41.35%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int openCount = 0, closeCount = 0;
-        for(char c : s){
-            if(c == '('){
+        for(char c : s)
+            if(c == '(')
                 openCount++;
-            } else if(c == ')' && openCount > 0){
+            else if(c == ')' && openCount > 0)
                 openCount--;
-            } else if(c == ')' && openCount <= 0){
+            else if(c == ')' && openCount <= 0)
                 closeCount++;
-            }
-        }
         return openCount + closeCount;
-    }
+    }   
 };
