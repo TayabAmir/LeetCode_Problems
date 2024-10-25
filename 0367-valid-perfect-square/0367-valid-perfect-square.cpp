f4247b6231945f3a6ff069d774e7cbf10ac72197
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-10-25 17:21:35 +0500

    Time: 0 ms (100%), Space: 7.4 MB (50.24%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        for(int i = 1; i <= pow(num,0.5); i++){
-            if(i*i==num) return true;
-            else if(i*i>num) break;
-        }
-    return false;  
+        double ans = pow(num,0.5);
+        return ans == (int)ans;  
     }
 };
