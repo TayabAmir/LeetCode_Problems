78b392698f8927aeb27d4ba0b4d646c60b1915f0
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-11-06 19:55:03 +0500

    Time: 3 ms (49.64%), Space: 31.2 MB (63.03%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& arr) {
-        for(int i = arr.size()- 1; i >= 0; i--){
-            for(int j = 0; j < i; j++){
-                if(__builtin_popcount(arr[j]) == __builtin_popcount(arr[j+1]) && (arr[j] > arr[j+1])){
+        for(int i = arr.size()- 1; i >= 0; i--)
+            for(int j = 0; j < i; j++)
+                if(__builtin_popcount(arr[j]) == __builtin_popcount(arr[j+1]) && (arr[j] > arr[j+1]))
                     swap(arr[j], arr[j+1]);
-                }
-            }
-        }
-        for(int i = 1; i < arr.size(); i++){
-            if(arr[i] < arr[i-1]){
+        for(int i = 1; i < arr.size(); i++)
+            if(arr[i] < arr[i-1])
                 return false;
-            }
-        }
         return true;
     }
 };
