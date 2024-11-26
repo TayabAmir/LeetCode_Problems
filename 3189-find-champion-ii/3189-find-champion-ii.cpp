c7b231551f23b247a1243f9d357ea65c606966f2
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-11-26 20:15:28 +0500

    Time: 62 ms (14.49%), Space: 104.8 MB (19.54%) - LeetHub

---
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> inDegrees(n,0);
+        for(auto e : edges){
+            inDegrees[e[1]]++;
+        }
+        int winner, count = 0;
+        for(int i = 0; i < n; i++)
+            if(!inDegrees[i]) winner = i, count++;
+        return (count == 1) ? winner : -1;
+    }
+};
