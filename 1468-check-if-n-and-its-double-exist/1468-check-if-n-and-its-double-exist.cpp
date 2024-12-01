13b634c0c750dbeeb3321ec57d9ff31e7e0c010e
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2024-12-01 21:42:05 +0500

    Time: 3 ms (24.95%), Space: 14.9 MB (10.47%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        for(int i = 0; i < arr.size(); i++){
-            for(int j = i+1; j < arr.size(); j++){
-                if(arr[i] == arr[j] * 2 || arr[j] == arr[i] * 2) return true;
+        unordered_map<int,int> m;
+        for(int ele : arr) m[ele]++;
+
+        for(int ele : arr){
+            if(ele != 0){
+                if(m.find(ele*2) != m.end()) return true;
+            } else {
+                if(m[0] >= 2) return true;
             }
         }
         return false;
