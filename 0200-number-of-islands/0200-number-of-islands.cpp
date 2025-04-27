668449c9455681f03a37713c83fda5eb57355e7c
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2025-04-27 19:15:35 +0500

    Time: 33 ms (26.06%), Space: 23.4 MB (30.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     void bfs(vector<vector<char>>& grid, int i, int j) {
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        grid[i][j] = '0';
-        int arr[] = {0, 1, 0, -1, 0};
-        int r, c, nr, nc;
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        grid[i][j] = 0;
+        int arr[] = {0, 1, 0, -1, 0},r,c,nr,nc;
 
-        while (!q.empty()) {
-            auto p = q.front();
+        while(!q.empty()){
+            r = q.front().first, c = q.front().second;
             q.pop();
-            r = p.first, c = p.second;
-            
-            for (int i = 0; i < 4; i++) {
-                nr = r + arr[i];
-                nc = c + arr[i + 1];
-                if (0 <= nr && nr < grid.size() && 0 <= nc && nc < grid[0].size() && grid[nr][nc]=='1'){
-                    q.push({nr, nc});
+
+            for(int i = 0; i < 4; i++){
+                nr = r + arr[i], nc = c + arr[i+1];
+                if(0 <= nr && nr < grid.size() and 0 <= nc and nc < grid[0].size() and grid[nr][nc] == '1'){
+                    q.push({nr,nc});
                     grid[nr][nc] = '0';
                 }
             }
         }
+
     }
     int numIslands(vector<vector<char>>& grid) {
         int out = 0;
