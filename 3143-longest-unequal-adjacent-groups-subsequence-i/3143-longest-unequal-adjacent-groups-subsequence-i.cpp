b7eab83b2ab42c5b7d234b6a5e9ec1441771485c
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2025-05-15 10:39:53 +0500

    Time: 0 ms (100%), Space: 29.9 MB (71.98%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     vector<string> getLongestSubsequence(vector<string>& w, vector<int>& g) {
-        int x = g[0];
-        vector<string> ans;
-        ans.push_back(w[0]);
-
-        for(int i = 1; i < w.size(); i++){
-            if((g[i] ^ x) == 1){
-                x = !x;
-                ans.push_back(w[i]);
-            }
-        }
+        vector<string> ans = {w[0]};
+        for(int i = 1, x = g[0]; i < w.size(); i++)
+            if((g[i] ^ x) == 1) x = !x, ans.push_back(w[i]);
         return ans;
     }
 };
