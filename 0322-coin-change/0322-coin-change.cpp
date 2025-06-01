93f7f2a9130e67e1fc344d862181acc2d7decbde
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2025-06-01 23:58:42 +0500

    Time: 170 ms (7.48%), Space: 56.3 MB (7.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int rec(int lvl, int amount, vector<int>& coins, vector<vector<int>> &dp){
-        if(amount == 0) return 0;
-        if(lvl == -1) return INT_MAX;
-        
-        if(dp[lvl][amount] != -1) return dp[lvl][amount];
-        int ans = rec(lvl-1, amount, coins, dp); // Dont take 
-        if(coins[lvl] <= amount) // Take
-        {
-            int res = rec(lvl, amount-coins[lvl], coins, dp);  
-            if(res != INT_MAX)
-                ans = min(ans, 1+res);
-        }
-        return dp[lvl][amount] = ans;
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int res = rec(coins.size()-1, amount, coins, dp);
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        function<int(int, int)> rec = [&](int lvl, int amount) -> int {
+            if(lvl == n) return INT_MAX;
+            if(amount == 0) return 0;
+
+            if(dp[lvl][amount] != -1) return dp[lvl][amount];
+
+            int skip = rec(lvl+1, amount);
+            if(coins[lvl] <= amount){
+                int res = rec(lvl, amount-coins[lvl]);
+                if(res != INT_MAX){
+                    skip = min(skip, 1+res);
+                }
+            }
+            return dp[lvl][amount] = skip;
+        };
+        int res = rec(0, amount);
         return (res != INT_MAX) ? res : -1;
     }
 };
