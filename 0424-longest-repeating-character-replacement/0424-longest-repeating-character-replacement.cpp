90da64a3e5cfad52f560b782d01f470760490dfd
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2025-07-04 10:51:04 +0500

    Time: 2 ms (75.74%), Space: 11 MB (40.65%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l = 0, r = 0, len = 0;
+        int l = 0, r = 0, len = 0, sum = 0, maxi = 0;
         vector<int> freq(26, 0);
-        function<int()> sum = [&]()->int{
-            int a = 0;
-            for(int ele : freq) a += ele;
-            return a;
-        };
+        // function<int()> sum = [&]()->int{
+        //     int a = 0;
+        //     for(int ele : freq) a += ele;
+        //     return a;
+        // };
         while(r < s.size()){
             freq[s[r]-'A']++;
-            int maxi = *max_element(freq.begin(), freq.end());
-            int left = sum() - maxi;
-            if(left > k) freq[s[l]-'A']--, l++;
+            sum += 1;
+            maxi = max(maxi, freq[s[r]-'A']);
+            int left = sum - maxi;
+            if(left > k) freq[s[l]-'A']--, sum--, l++;
             else len = max(len, r-l+1);
             r++;
         }
