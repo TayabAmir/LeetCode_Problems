f40aeb1e51cca5c5a37739d09404aa73c243ebd6
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2025-07-06 14:37:46 +0500

    Time: 0 ms (100%), Space: 12.4 MB (31.23%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int hIndex(vector<int>& c) {
-        int o = 0;
-        for(int i = 0; i <= c.size(); i++){
-            int co = 0;
-            for(int j = 0; j < c.size(); j++)
-                if(c[j] >= i) co++;
+        sort(c.begin(), c.end());
+        int n = c.size(), o = 0, maxi = c[n-1];
+
+        function<int(int)> bin = [&](int h) -> int {
+            int l = 0, r = c.size();
+            while (l < r) {
+                int m = l + (r - l) / 2;
+                (c[m] < h) ? l = m + 1 : r = m;
+            }
+            return l;
+        };
+
+        for(int i = 0; i <= maxi; i++){
+            int co = n - bin(i);
             if(i == min(co,i) && i > o) o = i;
         }
         return o;
