4cbe0494a966e9059513588c2c4ee16227db2f69
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2025-07-08 18:35:25 +0500

    Time: 0 ms (100%), Space: 11.9 MB (28.11%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& o) {
         int m = o.size(), n = o[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        set<pair<int,int>> st;
+        if(o[0][0] || o[m-1][n-1]) return 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
         for(int i = 0; i < m; i++)
             for(int j = 0; j < n; j++)
-                if(o[i][j]) st.insert({i, j});
-
-        function<int(int,int)> rec = [&](int i, int j) -> int{
-            if(i >= m || j >= n || st.find({i,j}) != st.end()) return 0;
-            if(i == m - 1 && j == n - 1) return 1;
-            if(dp[i][j] != -1) return dp[i][j];
-            return dp[i][j] = rec(i+1,j) + rec(i,j+1);
-        };
-        return rec(0,0);
+                if(!o[i][j]){
+                    if(i > 0)
+                        dp[i][j] = dp[i-1][j]; 
+                    if(j > 0)
+                        dp[i][j] += dp[i][j-1];
+                }
+        return dp[m-1][n-1];
     }
 };
