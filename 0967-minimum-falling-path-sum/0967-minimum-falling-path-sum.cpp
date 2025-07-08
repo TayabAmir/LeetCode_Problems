e74ac09093cd3aec1a53fb6aab6e74c186a26793
Muhammad Tayyab <144408356+TayabAmir@users.noreply.github.com>
2025-07-08 20:57:30 +0500

    Time: 0 ms (100%), Space: 14.9 MB (76.91%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -3,22 +3,14 @@ public:
     
     int minFallingPathSum(vector<vector<int>>& mat) {
         int n = mat.size();
-        vector<vector<int>> dp(n, vector<int>(n, -101));
-        function<int(int,int)> rec = [&](int i, int j) -> int {
-            if(i >= n || j < 0 || j >= n) return INT_MAX;
-            if(i == n-1) return mat[i][j]; 
-
-            if(dp[i][j] != -101) return dp[i][j];
-
-            int ans = INT_MAX;
-            for(int curr= -1; curr <= 1; curr++){
-                ans = min(ans, rec(i+1, j+curr));
+        vector<vector<int>> dp = mat;
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = mat[i][j] + dp[i+1][j];
+                if(j-1 >= 0) dp[i][j] = min(dp[i][j], mat[i][j] +dp[i+1][j-1]);
+                if(j+1 < n) dp[i][j] = min(dp[i][j], mat[i][j] + dp[i+1][j+1]);
             }
-            return dp[i][j] = mat[i][j] + ans;
-        };
-        int ans = INT_MAX;
-        for(int i = 0; i < n; i++)
-            ans = min(ans, rec(0,i));
-        return ans;
+        }
+        return *min_element(dp[0].begin(), dp[0].end());
     }
 };
